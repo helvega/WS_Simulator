0d5d65c49939c42d9a2470d596c1f7a4f7ffd47a
Helio <helvega@ucm.es>
2026-02-26 20:14:33 +0100

    Developed the main options and implemented de batch mode

--- src/simulator/launcher/Main.java
+++ src/simulator/launcher/Main.java
@@ -3,7 +3,10 @@ package simulator.launcher;
 import simulator.misc.Constants;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +20,13 @@ import org.apache.commons.cli.ParseException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import simulator.control.Controler;
 import simulator.factories.*;
 import simulator.misc.Utils;
 import simulator.model.Animal;
 import simulator.model.Region;
 import simulator.model.SelectionStrategy;
+import simulator.model.Simulator;
 import simulator.view.SimpleObjectViewer;
 
 public class Main {
@@ -49,12 +54,16 @@ public class Main {
 	// default values for some parameters
 	//
 	private final static Double DEFAULT_TIME = 10.0; // in seconds
+	private final static Double DEFAULT_DTIME = 0.03; // in seconds
 
 	// some attributes to stores values corresponding to command-line parameters
 	//
 	private static Double time = null;
+	private static Double dt = null;
 	private static String inFile = null;
-	private static ExecMode mode = ExecMode.BATCH;
+	private static String outFile = null;
+	private static boolean sv = true;
+ 	private static ExecMode mode = ExecMode.BATCH;
 	
 	private static Factory <SelectionStrategy> SsFactory;
 	private static Factory <Animal> AnFactory;
@@ -73,7 +82,10 @@ public class Main {
 			CommandLine line = parser.parse(cmdLineOptions, args);
 			parseHelpOption(line, cmdLineOptions);
 			parseInFileOption(line);
+			parseOutFileOption(line);
 			parseTimeOption(line);
+			parseDtOption(line);
+			parseSvOption(line);
 
 			// if there are some remaining arguments, then something wrong is
 			// provided in the command line!
@@ -95,17 +107,28 @@ public class Main {
 
 	private static Options buildOptions() {
 		Options cmdLineOptions = new Options();
-
+		
+		//time per step
+		cmdLineOptions.addOption(Option.builder("dt").longOpt("delta-time").hasArg().desc("A double representing actual time, in  \n"
+				+ "                             seconds, per simulation step. Default value: 0.03. ").build());
 		// help
 		cmdLineOptions.addOption(Option.builder("h").longOpt("help").desc("Print this message.").build());
 
 		// input file
 		cmdLineOptions.addOption(Option.builder("i").longOpt("input").hasArg().desc("A configuration file.").build());
+		
+		//output file
+		cmdLineOptions.addOption(Option.builder("o").longOpt("ouput").hasArg().desc("Output File where output is written.").build());
+		
+		//simple viewer
+		cmdLineOptions.addOption(Option.builder("sv").longOpt("simple-viewer").desc("Show the viewer window in console mode.").build());
 
 		// steps
 		cmdLineOptions.addOption(Option.builder("t").longOpt("time").hasArg()
 				.desc("A real number representing the total simulation time in seconds. Default value: "
 						+ DEFAULT_TIME + ".").build());
+		
+		
 
 		return cmdLineOptions;
 	}
@@ -117,11 +140,18 @@ public class Main {
 			System.exit(0);
 		}
 	}
+	
+	private static void parseOutFileOption(CommandLine line) throws ParseException {
+		outFile = line.getOptionValue("o");
+		if (mode == ExecMode.BATCH && outFile == null) {
+			throw new ParseException("In batch mode an output file is required to run the Controller");
+		}
+	}
 
 	private static void parseInFileOption(CommandLine line) throws ParseException {
 		inFile = line.getOptionValue("i");
 		if (mode == ExecMode.BATCH && inFile == null) {
-			throw new ParseException("In batch mode an input configuration file is required");
+			throw new ParseException("In batch mode an input configuration file is required to initialize the simulation");
 		}
 	}
 
@@ -132,6 +162,25 @@ public class Main {
 			assert (time >= 0);
 		} catch (Exception e) {
 			throw new ParseException("Invalid value for time: " + t);
+		}
+	}
+	
+	private static void parseDtOption(CommandLine line) throws ParseException {
+		String t = line.getOptionValue("dt", DEFAULT_DTIME.toString());
+		try {
+			dt = Double.parseDouble(t);
+			assert (dt >= 0);
+		} catch (Exception e) {
+			throw new ParseException("Invalid value for dt: " + t);
+		}
+	}
+	
+	private static void parseSvOption(CommandLine line) throws ParseException {
+		String s = line.getOptionValue("sv", "true");
+		try {
+			sv = Boolean.parseBoolean(s);
+		} catch (Exception e) {
+			throw new ParseException("Invalid value for sv: " + s);
 		}
 	}
 
@@ -158,7 +207,21 @@ public class Main {
 
 	private static void startBatchMode() throws Exception {
 		InputStream is = new FileInputStream(new File(inFile));
-		// TODO ...
+		
+		JSONObject jo = loadJSONFile(is);
+		int cols = jo.getInt("cols");
+		int rows = jo.getInt("rows");
+		int width = jo.getInt("width");
+		int height = jo.getInt("height");
+		
+		Simulator sim = new Simulator(cols, rows, width, height, AnFactory, RnFactory);
+		Controler controler = new Controler(sim);
+		JSONObject data = jo.has("data") ? jo.getJSONObject("data") : new JSONObject();
+		controler.loadData(data);
+		OutputStream out = new FileOutputStream(outFile);
+		PrintStream p = new PrintStream(out);
+		controler.run(time, dt, sv, p);
+		out.close();
 	}
 
 	private static void startGUIMode() throws Exception {
