af3fb91007f033da8ba63790f999ed75f2fe0efe
Matrejo <migtrejo@ucm.es>
2026-04-13 19:07:10 +0200

    Update ControlPanel.java

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -1,13 +1,16 @@
 package simulator.view;
 
 import java.awt.BorderLayout;
+import java.io.File;
 
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
 
 import simulator.control.Controller;
 
@@ -19,7 +22,8 @@ class ControlPanel extends JPanel {
 	  private JToolBar toolaBar;
 	  private JFileChooser fc;
 	  private boolean stopped = true; // used for the buttons run/stop
-	  private JButton quitButton;
+	  private JButton quitButton, openButton, viewerButton, regionsButton, runButton, stopButton;
+	  private double dt;
 
 	  // TODO add fields here
 
@@ -36,7 +40,55 @@ class ControlPanel extends JPanel {
 	    // TODO create the different widgets (buttons, etc.) and add them to toolaBar.
 	    //      All of them must have their corresponding tooltip. You can use
 	    //      this.toolaBar.addSeparator() to add the vertical separator line
-	    //      between the components that need it.
+	    //      between the components that need it
+	    
+	    // Button to open files
+	    this.toolaBar.add(Box.createGlue()); // this aligns the button to the right
+	    this.openButton = new JButton();
+	    this.openButton.setToolTipText("Open");
+	    this.openButton.setIcon(new ImageIcon("resources/icons/open.png"));
+	    this.openButton.addActionListener((e) -> openFiles());
+	    this.openButton.setToolTipText("Opens a file from which to load the data");
+	    this.toolaBar.add(openButton);
+	    this.toolaBar.addSeparator();
+	    
+	    // Button to open the map
+	    this.toolaBar.add(Box.createGlue()); // this aligns the button to the right
+	    this.viewerButton = new JButton();
+	    this.viewerButton.setToolTipText("Viewer");
+	    this.viewerButton.setIcon(new ImageIcon("resources/icons/viewer.png"));
+	 // TODO create an instance of MapWindow
+	    this.viewerButton.addActionListener((e) -> ViewUtils.quit(this));
+	    this.viewerButton.setToolTipText("Opens a map representation");
+	    this.toolaBar.add(viewerButton);
+	    
+	    // Button to open the region viewer
+	    this.toolaBar.add(Box.createGlue()); // this aligns the button to the right
+	    this.regionsButton = new JButton();
+	    this.regionsButton.setToolTipText("Regions");
+	    this.regionsButton.setIcon(new ImageIcon("resources/icons/regions.png"));
+	    this.regionsButton.addActionListener((e) -> this.changeRegionsDialog.open(ViewUtils.getWindow(this)));
+	    this.regionsButton.setToolTipText("Opens the region viewer");
+	    this.toolaBar.add(regionsButton);
+	    this.toolaBar.addSeparator();
+	    
+	    // Button to start the simulation
+	    this.toolaBar.add(Box.createGlue()); // this aligns the button to the right
+	    this.runButton = new JButton();
+	    this.runButton.setToolTipText("Run");
+	    this.runButton.setIcon(new ImageIcon("resources/icons/run.png"));
+	    this.runButton.addActionListener((e) -> run());
+	    this.runButton.setToolTipText("Runs the simulation");
+	    this.toolaBar.add(runButton);
+	    
+	    // Button to stop the simulation
+	    this.toolaBar.add(Box.createGlue()); // this aligns the button to the right
+	    this.stopButton = new JButton();
+	    this.stopButton.setToolTipText("Stop");
+	    this.stopButton.setIcon(new ImageIcon("resources/icons/stop.png"));
+	    this.stopButton.addActionListener((e) -> this.stopped = true);
+	    this.stopButton.setToolTipText("Stops the simulation if it is already running");
+	    this.toolaBar.add(stopButton);
 
 	    // Quit Button
 	    this.toolaBar.add(Box.createGlue()); // this aligns the button to the right
@@ -45,16 +97,64 @@ class ControlPanel extends JPanel {
 	    this.quitButton.setToolTipText("Quit");
 	    this.quitButton.setIcon(new ImageIcon("resources/icons/exit.png"));
 	    this.quitButton.addActionListener((e) -> ViewUtils.quit(this));
+	    this.quitButton.setToolTipText("Closes the control panel");
 	    this.toolaBar.add(quitButton);
 
-	    // TODO Initialize this.fc with an instance of JFileChooser. To make it always
-	    //      open in the examples folder you can use:
-	    //
-	    //      this.fc.setCurrentDirectory(new File(System.getProperty("user.dir") + "/resources/examples"));
+	    fc = new JFileChooser();
+	    this.fc.setCurrentDirectory(new File(System.getProperty("user.dir") + "/resources/examples"));
 	   
 	    // TODO Initialize this.changeRegionsDialog with a corresponding instance.
 
 	  } 
+	  
+	  private void enableButtons(boolean enabled) {
+		  openButton.setEnabled(enabled);
+	      viewerButton.setEnabled(enabled);
+	      regionsButton.setEnabled(enabled);
+	      runButton.setEnabled(enabled);
+	      quitButton.setEnabled(enabled);
+	  }
+	  
+	  private void run() {
+		  this.stopped = false;
+		  enableButtons(stopped);
+		  
+	      // TODO get n and dt
+	      runSim(0, dt);
+	  }
+	  
+	  private void runSim(int n, double dt) {
+		  if (n > 0 && !this.stopped) {
+		    try {
+		      this.ctrl.advance(dt);
+		      SwingUtilities.invokeLater(() -> runSim(n - 1, dt));
+		      
+		    } catch (Exception e) {
+		      // TODO think of an error message
+		      ViewUtils.showErrorMsg("Idk what to say here");
+		      
+		      this.stopped = true;
+		      enableButtons(stopped);
+		    }
+		  } else {
+		      this.stopped = true;
+		      enableButtons(stopped);
+		  }
+		}
+	  
+	  private void openFiles() {
+		  int returnVal = this.fc.showOpenDialog(ViewUtils.getWindow(this));
+		  if (returnVal == JFileChooser.APPROVE_OPTION) {
+			  File file = fc.getSelectedFile();
+			  System.out.println("Loading: " + file.getName());
+			  
+			  // TODO convert to JSon
+			  this.ctrl.reset(returnVal, returnVal, returnVal, returnVal);
+		  } else {
+			  System.out.println("Load cancelled by user.");
+		  }
+	  }
+	  
 	  // TODO The rest of methods.
 	}
 
